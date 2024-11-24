417c6e09473fbee3213bae1fc71571db59fc6ab8
sukaruta <sukaruta12@gmail.com>
2024-11-25 01:46:23 +0800

    update about page

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -14,6 +14,9 @@ function AboutPage() {
             <InfoBox headerText={"How do I use the website?"} paragraphText={"You can scroll down on the home page to see a list of recently happened earthquakes displayed to you. You can see details like where the earthquake took place on a globe, and how you can donate to help relief people affected by said earthquake."}/>
             <InfoBox headerText={"How will Quakewatch reduce the damage and casualties caused by earthquakes?"} paragraphText={"Though Quakewatch can't prevent earthquakes, it does aim to minimize damage and casualties through exposing more ways for people to donate and help earthquake relief organizations. Through the use of increased donations and exposure, places affected by earthquakes can bounce back quicker because they will have a lot of focus on them to rebuild and accommodate and help people affected by it."} />
             <InfoBox headerText={"The Website's Future"} paragraphText={"As the website currently stands, it is only still a prototype/shell of it's proposed features, a website that displays the location of earthquakes and gives you options to donate to relief people affected from said earthquakes. If you've noticed the Donate button does not work, that is simply because it is not implemented yet since it would be far too complex given my timeframe to do. I would have to run country region detection checks then evaluate organizations that do run earthquake donations to said country. That said, if this project ever regains momentum, I will probably release those proposed features."} />
+
+
+            <p className="text-xs">Quakewatch has been acquired by D.A.M.M Innovations (unfortunately, bad business decisions from here on out)</p>
         </div>
     )
 }
